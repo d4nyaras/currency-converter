774b1b830d0d769d13c8db9da4b44156146bdc2a
Donya <d4nyaras@gmail.com>
2025-06-01 15:14:53 +0330

    Add: Currency converter component + navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import CurrencyConverter from "@/components/CurrencyConverter";
-import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col h-screen bg-[#f4f6f8]">
+      <Navbar />
       <CurrencyConverter />
     </div>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="text-purple-600  py-4 text-center ">
+      <h2 className="font-semibold text-2xl ">Currency Converter</h2>
+    </div>
+  );
+}
